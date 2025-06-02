eb8e5518f2d7f98f47a5ec4c9141b10e90f6efa4
wainou06 <wainou.6@gmail.com>
2025-06-02 11:50:54 +0900

    up

--- React_Quiz_Practice/ch02/src/App.js
+++ React_Quiz_Practice/ch02/src/App.js
@@ -1,14 +1,14 @@
 import './App.css'
-import Quiz01 from './Quiz01'
+//import Quiz01 from './Quiz01'
 //import Quiz02 from './Quiz02'
-// import Quiz03 from './Quiz03'
-// import Quiz04 from './Quiz04'
+//import Quiz03 from './Quiz03'
+import Quiz04 from './Quiz04'
 
 function App() {
-   return <Quiz01 />
+   //return <Quiz01 />
    //return <Quiz02 />
    //return <Quiz03 />
-   // return <Quiz04 />
+   return <Quiz04 />
 }
 
 export default App
--- React_Quiz_Practice/ch02/src/Quiz03.js
+++ React_Quiz_Practice/ch02/src/Quiz03.js
@@ -5,7 +5,7 @@ function Quiz03() {
    return (
       <div>
          <p>오늘의 날씨는 맑음입니다.</p>
-         <p>오늘의 강수확률은 입니다.</p>
+         <p>오늘의 강수확률은 {weather && rain}입니다.</p>
       </div>
    )
 }
