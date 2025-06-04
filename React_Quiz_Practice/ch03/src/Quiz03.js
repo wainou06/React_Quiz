7b1f04dca7a87cbd4565cc133c1a3dcdfada7972
wainou06 <wainou.6@gmail.com>
2025-06-04 13:58:36 +0900

    up

--- React_Quiz_Practice/ch03/src/Quiz03.js
+++ React_Quiz_Practice/ch03/src/Quiz03.js
@@ -2,18 +2,18 @@ import React, { useState } from 'react'
 import Quiz03_sub from './Quiz03_sub'
 
 function Quiz03() {
-  const [message, setMessage] = useState("안녕하세요!")
+   const [message, setMessage] = useState('안녕하세요!')
 
-  const updateMessage = () => {
-    setMessage("반갑습니다!")
-  }
+   const updateMessage = () => {
+      setMessage('반갑습니다!')
+   }
 
-  return (
-    <div>
-      <h1>{message}</h1>
-      <Quiz03_sub updateMessage={updateMessage} />
-    </div>
-  )
+   return (
+      <div>
+         <h1>{message}</h1>
+         <Quiz03_sub updateMessage={updateMessage} />
+      </div>
+   )
 }
 
 export default Quiz03
