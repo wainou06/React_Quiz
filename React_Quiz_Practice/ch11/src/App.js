cd920e8d2977a1446a76b1e12750912ede97c0dd
wainou06 <wainou.6@gmail.com>
2025-06-16 13:20:16 +0900

    up

--- React_Quiz_Practice/ch11/src/App.js
+++ React_Quiz_Practice/ch11/src/App.js
@@ -2,13 +2,13 @@
 import Quiz0101 from './Quiz01_1/Login'
 import storeQuiz0101 from './Quiz01_1/store'
 import { Provider } from 'react-redux'
-/* import storeQuiz02 from './Quiz02/store2'
+import storeQuiz02 from './Quiz02/store2'
 import CounterComponent from './Quiz02/CounterComponent'
 import CounterComponent2 from './Quiz02/CounterComponent2'
 import storeQuiz03 from './Quiz03/store'
-import TodoComponent from './Quiz03/TodoComponent' */
-import storeQuiz04 from './Quiz04/store'
-import TodoComponent from './Quiz04/TodoComponent'
+import TodoComponent from './Quiz03/TodoComponent'
+/* import storeQuiz04 from './Quiz04/store'
+import TodoComponent from './Quiz04/TodoComponent' */
 
 function App() {
    // return <Quiz01 />
@@ -22,16 +22,16 @@ function App() {
          <CounterComponent2 />
       </Provider>
    ) */
-   /*  return (
+    return (
       <Provider store={storeQuiz03}>
          <TodoComponent />
       </Provider>
-   ) */
-   return (
+   )
+   /* return (
       <Provider store={storeQuiz04}>
          <TodoComponent />
       </Provider>
-   )
+   ) */
 }
 
 export default App
