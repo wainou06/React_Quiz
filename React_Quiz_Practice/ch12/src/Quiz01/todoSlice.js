325262d07ed8ec32d3ab5bec44d5e41cf842ca64
wainou06 <wainou.6@gmail.com>
2025-06-16 18:14:32 +0900

    up

--- React_Quiz_Practice/ch12/src/Quiz01/todoSlice.js
+++ React_Quiz_Practice/ch12/src/Quiz01/todoSlice.js
@@ -9,6 +9,7 @@ const todoSlice = createSlice({
          state.push({ id: Date.now(), text: action.payload })
       },
       removeTodo: (state, action) => {
+         // filter, map을 사용시 return을 하지 않으면 state값을 바꾸는게 아니라 배열만 만들어주므로 반드시 리턴
          return state.filter((todo) => todo.id !== action.payload)
       },
    },
